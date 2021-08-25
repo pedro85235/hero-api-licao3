75f9dcfdc7b43c7da3a4bf880eefcdb8bd11f216
[pedro85235] <[pedroalvesmota85235@gmail.com]>
2021-08-24 22:09:37 -0300

    alteramos o banco

--- includes/Constants.php
+++ includes/Constants.php
@@ -2,6 +2,6 @@
 
 
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'etecia');
-	define('DB_PASS', '123456');
+	define('DB_USER', 'etec');
+	define('DB_PASS', '1234567');
 	define('DB_NAME', 'android');
